2e5e672ed244f31b064f6ceed5991e80b77a17da
Daniel Rivas <danielrivasmd@gmail.com>
2025-04-26 13:19:50 -0600

    add format repo size function

--- cmd/cmdGuard.go
+++ cmd/cmdGuard.go
@@ -96,9 +96,12 @@ var guardCmd = &cobra.Command{
     checkErr(err)
     fmt.Println("Repo Remote: ", remoteURL)
 
-    repoSize, err := calculateRepoSize(repo)
-    checkErr(err)
-    fmt.Printf("Repo Size: %d bytes\n", repoSize)
+		repoSize, err := calculateRepoSize(repo)
+		checkErr(err)
+		fmt.Printf("Repo Size: %d bytes\n", repoSize)
+
+		humanReadableSize := formatRepoSize(repoSize)
+		fmt.Println("Human-readable repo size:", humanReadableSize)
 
     commitFrequency, err := calculateCommitFrequency(repo)
     checkErr(err)
@@ -187,6 +190,25 @@ func calculateRepoSize(repo string) (int64, error) {
         return err
     })
     return size, err
+}
+
+func formatRepoSize(size int64) string {
+    const (
+        KB = 1024
+        MB = KB * 1024
+        GB = MB * 1024
+    )
+
+    switch {
+    case size >= GB:
+        return fmt.Sprintf("%.2f GB", float64(size)/float64(GB))
+    case size >= MB:
+        return fmt.Sprintf("%.2f MB", float64(size)/float64(MB))
+    case size >= KB:
+        return fmt.Sprintf("%.2f KB", float64(size)/float64(KB))
+    default:
+        return fmt.Sprintf("%d bytes", size)
+    }
 }
 
 // Computes commit frequency, grouping by time intervals
