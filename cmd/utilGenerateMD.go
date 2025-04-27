d90da8639ab0706d6666e2d4b0d0ca824604ab85
Daniel Rivas <danielrivasmd@gmail.com>
2025-04-27 13:46:21 -0600

    udpate generate markdown util

--- cmd/utilGenerateMD.go
+++ cmd/utilGenerateMD.go
@@ -12,44 +12,58 @@ import (
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// create Markdown table
 func generateMD(stats RepoStats, repoName string) string {
 	var builder strings.Builder
 
 	// header
-	builder.WriteString("| Repo        | Language | Age    | Commits | Remote                                   | Lines  | Size    | Frequency |")
+	builder.WriteString("| Repo        | Remote                                        | Commit | Age    | Language | Lines  | Size    | Mean |")
 	for i := 1; i <= 12; i++ {
 		builder.WriteString(fmt.Sprintf("  %02d |", i))
 	}
 	builder.WriteString("\n")
 
 	// separator
-	builder.WriteString("|-------------|----------|--------|---------|------------------------------------------|--------|---------|-----------|")
+	builder.WriteString("|-------------|-----------------------------------------------|--------|--------|----------|--------|---------|------|")
 	for i := 1; i <= 12; i++ {
 		builder.WriteString("-----|")
 	}
 	builder.WriteString("\n")
 
-	// data
-	// frequency := "Monthly Breakdown"
+	// calculate average commits per month
+	repoAgeMonths := calculateRepoAgeInMonths(stats.Age)
+	averageCommits := 0
+	if repoAgeMonths > 0 {
+		averageCommits = stats.Commits / repoAgeMonths
+	}
+
+	// data row
 	builder.WriteString(fmt.Sprintf(
-		"| %-11s | %-8s | %-6s | %-7d | %-40s | %-6d | %-7d | %-9s |",
+		"| %-11s | %-45s | %-6d | %-6s | %-8s | %-6d | %-7s | %-4d |",
 		repoName,
-		stats.Language,
-		stats.Age,
-		stats.Count,
 		stats.Remote,
+		stats.Commits,
+		stats.Age,
+		stats.Language,
 		stats.Lines,
-		stats.Files,
-		formatRepoSize(stats.Size),
+		stats.Size,
+		averageCommits, // average commits per month
 	))
 	for i := 1; i <= 12; i++ {
-		month := fmt.Sprintf("%d-%02d", time.Now().Year(), i)
-		builder.WriteString(fmt.Sprintf(" %-3d |", stats.Frequency[month]))
+		month := fmt.Sprintf("%d-%02d", time.Now().Year(), i) // format: YYYY-MM
+		monthCommits := stats.Frequency[month]                // month frequency
+		builder.WriteString(fmt.Sprintf(" %-3d |", monthCommits))
 	}
 	builder.WriteString("\n")
 
 	return builder.String()
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+func calculateRepoAgeInMonths(age string) int {
+	years, months := 0, 0
+	fmt.Sscanf(age, "%dy %dm", &years, &months) // parse "Xy Ym" format
+	return (years * 12) + months
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
