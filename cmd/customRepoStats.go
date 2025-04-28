bd8e487084b960562d25f27f6aa14ef7bb69b3d7
Daniel Rivas <danielrivasmd@gmail.com>
2025-04-27 23:44:25 -0600

    omit readme & license popualte repo stats function

--- cmd/customRepoStats.go
+++ cmd/customRepoStats.go
@@ -5,7 +5,6 @@ package cmd
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 import (
-	"fmt"
 	"strconv"
 )
 
@@ -28,43 +27,43 @@ func populateRepoStats(repo string, year int) (RepoStats, error) {
 	// initialize RepoStats
 	stats := RepoStats{}
 
-	// list files
-	files, ε := listFiles(repo)
-	if ε != nil {
-		return stats, ε
-	}
+	// // list files
+	// files, ε := listFiles(repo)
+	// if ε != nil {
+	// 	return stats, ε
+	// }
 
-	// declare switches
-	readmeFound := false
-	licenseFound := false
+	// // declare switches
+	// readmeFound := false
+	// licenseFound := false
 
-	// iterate on files
-	for _, file := range files {
-		if file == "README.md" {
-			readmeFound = true
-			description, err := parseReadme(repo + "/" + file)
-			if err != nil {
-				return stats, err
-			}
-			fmt.Println("Extracted Description:\n", description)
-		}
+	// // iterate on files
+	// for _, file := range files {
+	// 	if file == "README.md" {
+	// 		readmeFound = true
+	// 		description, err := parseReadme(repo + "/" + file)
+	// 		if err != nil {
+	// 			return stats, err
+	// 		}
+	// 		fmt.Println("Extracted Description:\n", description)
+	// 	}
 
-		if file == "LICENSE" {
-			licenseFound = true
-			licenseType, err := detectLicense(repo + "/" + file)
-			if err != nil {
-				return stats, err
-			}
-			fmt.Println("License is: ", licenseType)
-		}
-	}
+	// 	if file == "LICENSE" {
+	// 		licenseFound = true
+	// 		licenseType, err := detectLicense(repo + "/" + file)
+	// 		if err != nil {
+	// 			return stats, err
+	// 		}
+	// 		fmt.Println("License is: ", licenseType)
+	// 	}
+	// }
 
-	if !readmeFound {
-		fmt.Println("README.md not found in the directory.")
-	}
-	if !licenseFound {
-		fmt.Println("LICENSE not found in the directory.")
-	}
+	// if !readmeFound {
+	// 	fmt.Println("README.md not found in the directory.")
+	// }
+	// if !licenseFound {
+	// 	fmt.Println("LICENSE not found in the directory.")
+	// }
 
 	// fetch repository metrics
 	tokeiOut, _, ε := captureExecCmd("tokei", "-C")
