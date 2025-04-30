6d4304cfb0e16fc488a1c6ec784de686adf939a9
Daniel Rivas <danielrivasmd@gmail.com>
2025-04-29 21:48:41 -0600

    update create dir util

--- cmd/utilCreateDir.go
+++ cmd/utilCreateDir.go
@@ -7,18 +7,29 @@ package cmd
 import (
 	"fmt"
 	"os"
+
+	"github.com/DanielRivasMD/horus"
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-func createDirectory(path string) error {
-	fmt.Printf("Attempting to create directory '%s'.\n", path)
-	err := os.MkdirAll(path, 0755)
-	if err != nil {
-		return fmt.Errorf("failed to create directory '%s': %w", path, err)
+// Example of a NotFoundAction to create a directory if it doesn't exist
+func createDir(dirPath string) horus.NotFoundAction {
+	return func(address string) error {
+		fmt.Printf("Attempting to create directory: %s\n", address)
+		err := os.Mkdir(address, 0755)
+		if err != nil {
+			return horus.NewCategorizedHerror(
+				"create directory",
+				"directory_creation_error",
+				"failed to create directory",
+				err,
+				map[string]any{"path": address},
+			)
+		}
+		fmt.Printf("Directory successfully created: %s\n", address)
+		return nil
 	}
-	fmt.Printf("Directory '%s' created successfully.\n", path)
-	return nil
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
