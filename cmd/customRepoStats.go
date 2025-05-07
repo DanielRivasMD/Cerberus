5e7b32c03b46484ed08e5dae0369dfca60174a43
Daniel Rivas <danielrivasmd@gmail.com>
2025-05-07 14:59:36 -0600

    update repo stats custom

--- cmd/customRepoStats.go
+++ cmd/customRepoStats.go
@@ -28,44 +28,6 @@ func populateRepoStats(year int) (RepoStats, error) {
 	// initialize RepoStats
 	stats := RepoStats{}
 
-	// // list files
-	// files, ε := listFiles(repo)
-	// if ε != nil {
-	// 	return stats, ε
-	// }
-
-	// // declare switches
-	// readmeFound := false
-	// licenseFound := false
-
-	// // iterate on files
-	// for _, file := range files {
-	// 	if file == "README.md" {
-	// 		readmeFound = true
-	// 		description, err := parseReadme(repo + "/" + file)
-	// 		if err != nil {
-	// 			return stats, err
-	// 		}
-	// 		fmt.Println("Extracted Description:\n", description)
-	// 	}
-
-	// 	if file == "LICENSE" {
-	// 		licenseFound = true
-	// 		licenseType, err := detectLicense(repo + "/" + file)
-	// 		if err != nil {
-	// 			return stats, err
-	// 		}
-	// 		fmt.Println("License is: ", licenseType)
-	// 	}
-	// }
-
-	// if !readmeFound {
-	// 	fmt.Println("README.md not found in the directory.")
-	// }
-	// if !licenseFound {
-	// 	fmt.Println("LICENSE not found in the directory.")
-	// }
-
 	// fetch repository metrics
 	tokeiOut, _, ε := captureExecCmd("tokei", "-C")
 	if ε != nil {
@@ -93,13 +55,6 @@ func populateRepoStats(year int) (RepoStats, error) {
 		return stats, ε
 	}
 	stats.Commits = commitCount
-
-	// define remote
-	remoteURL, ε := getRemote()
-	if ε != nil {
-		return stats, ε
-	}
-	stats.Remote = remoteURL
 
 	// define repo size
 	size, ε := repoSize()
