de04d4d605985b5432a78873b320c1706f8e798f
Daniel Rivas <danielrivasmd@gmail.com>
2025-05-22 12:32:31 +0200

    update list files util

--- cmd/utilListFiles.go
+++ cmd/utilListFiles.go
@@ -6,18 +6,30 @@ package cmd
 
 import (
 	"os"
+
+	"github.com/DanielRivasMD/horus"
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// listFiles returns slice filenames
+// listFiles returns a slice of filenames from the given directory.
+// If an error occurs while reading the directory, the error is wrapped
+// using horus.NewCategorizedHerror.
 func listFiles(directory string) ([]string, error) {
 	entries, err := os.ReadDir(directory)
-	checkErr(err)
+	if err != nil {
+		return nil, horus.NewCategorizedHerror(
+			"list files",
+			"io",
+			"failed to list files in directory",
+			err,
+			map[string]any{"directory": directory},
+		)
+	}
 
 	var files []string
 	for _, entry := range entries {
-		// ensures only regular files
+		// ensures only regular files are considered.
 		if entry.Type().IsRegular() {
 			files = append(files, entry.Name())
 		}
