5abc65b5c3ebfdd8487720cf58598eace0b360c7
Daniel Rivas <danielrivasmd@gmail.com>
2025-06-03 10:58:50 +0200

    add clone cmd

---
+++ cmd/cmdClone.go
@@ -0,0 +1,63 @@
+/*
+Copyright © 2025 Daniel Rivas <danielrivasmd@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"github.com/DanielRivasMD/horus"
+	"github.com/spf13/cobra"
+	"github.com/ttacon/chalk"
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Global declarations
+var (
+	csvFile   string
+	targetDir string
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// cloneCmd
+var cloneCmd = &cobra.Command{
+	Use:   "clone",
+	Short: "" + chalk.Yellow.Color("") + ".",
+	Long: chalk.Green.Color(chalk.Bold.TextStyle("Daniel Rivas ")) + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<danielrivasmd@gmail.com>")) + `
+`,
+
+	Example: `
+` + chalk.Cyan.Color("") + ` help ` + chalk.Yellow.Color("") + chalk.Yellow.Color("clone"),
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	Run: func(cmd *cobra.Command, args []string) {
+		err := cloneRepositoriesFromCSV(csvFile, targetDir)
+		horus.CheckErr(err)
+	},
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// execute prior main
+func init() {
+	rootCmd.AddCommand(cloneCmd)
+
+	// flags
+	cloneCmd.Flags().StringVarP(&csvFile, "csv", "", "", "File in csv format containing remote repositories")
+	cloneCmd.Flags().StringVarP(&targetDir, "directory", "", "", "Directory to clone repositories")
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
