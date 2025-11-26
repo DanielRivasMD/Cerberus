903eef19b34a3c7281c782b4dc8b1a68b0a4b8d0
Daniel Rivas <danielrivasmd@gmail.com>
2025-11-26 09:26:35 +0100

    refactor root

--- cmd/root.go
+++ cmd/root.go
@@ -19,6 +19,9 @@ package cmd
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 import (
+	"fmt"
+
+	"github.com/DanielRivasMD/domovoi"
 	"github.com/DanielRivasMD/horus"
 	"github.com/spf13/cobra"
 )
@@ -52,6 +55,7 @@ var (
 	output  string
 )
 
+// TODO: refactor as struct
 const (
 	repoLen = 25
 
@@ -68,5 +72,68 @@ const (
 
 	remoteLen = 95
 )
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// handleGit is a generic function that performs the ".git" check and then
+// dispatches to the appropriate report generation function.
+//
+// reportType should be "stats" or "describe".
+// repository is the path (or name) for the repo to report on.
+// year is used in stats reporting.
+// verbose controls extra output in domovoi.DirExist.
+func handleGit(reportType string, verbose bool) error {
+	// Check if the .git directory exists.
+	ok, err := domovoi.DirExist(".git", horus.NullAction(false), verbose)
+	if err != nil {
+		return err
+	}
+
+	var repoNames []string
+
+	if !ok {
+		// OUT branch: The ".git" directory does not exist.
+		// List the subdirectories from the given repository.
+		repoNames, err = domovoi.ListDirs(repository)
+		if err != nil {
+			return err
+		}
+		// Generate the report based on reportType.
+		switch reportType {
+		case "stats":
+			fmt.Println(generateStatsMD(repoNames, year))
+		case "describe":
+			fmt.Println(generateDescribeMD(repoNames))
+		case "remember":
+			fmt.Println(generateRememberMD(repoNames))
+		default:
+			return fmt.Errorf("unknown report type: %s", reportType)
+		}
+	} else {
+		// IN branch: The ".git" directory is present.
+		// If the repository is ".", then update it from the current directory.
+		if repository == "." {
+			repoName, err := domovoi.CurrentDir()
+			if err != nil {
+				return err
+			}
+			repository = repoName
+		}
+		// Create a slice with just this repository.
+		repoNames = append(repoNames, repository)
+		switch reportType {
+		case "stats":
+			fmt.Println(generateStatsMD(repoNames, year))
+		case "describe":
+			fmt.Println(generateDescribeMD(repoNames))
+		case "remember":
+			fmt.Println(generateRememberMD(repoNames))
+		default:
+			return fmt.Errorf("unknown report type: %s", reportType)
+		}
+	}
+
+	return nil
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
