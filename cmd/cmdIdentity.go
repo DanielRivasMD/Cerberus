1d9fad3a71476e06d6c6bdc71ec42e8d635ee253
Daniel Rivas <danielrivasmd@gmail.com>
2026-03-05 14:17:45 +0100

    refactor identity cmd

--- cmd/cmdIdentity.go
+++ cmd/cmdIdentity.go
@@ -16,55 +16,37 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 package cmd
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 import (
 	"fmt"
 
-	"github.com/DanielRivasMD/domovoi"
 	"github.com/spf13/cobra"
-	"github.com/ttacon/chalk"
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// declarations
-var (
-	iden = `In Greek mythology, ` + chalk.Yellow.Color("Cerberus") + `, ` + chalk.Dim.TextStyle("Κέρβερος") + `, often referred to as the hound of Hades, is a multi-headed dog
+const IDENT = `In Greek mythology, Cerberus, Κέρβερος, often referred to as the hound of Hades, is a multi-headed dog
 that guards the gates of the underworld to prevent the dead from leaving.
 
 He was the offspring of the monsters Echidna and Typhon, and was usually described as having three heads,
 a serpent for a tail, and snakes protruding from his body.
 
 Cerberus is primarily known for his capture by Heracles, the last of Heracles' twelve labours`
-)
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// identityCmd
-var identityCmd = &cobra.Command{
-	Use:   "identity",
-	Short: `Reveal identity`,
-	Long:  `Reveal identity`,
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	Run: func(κ *cobra.Command, args []string) {
-
-		domovoi.LineBreaks()
-		fmt.Println()
-
-		fmt.Println(iden)
-
-		domovoi.LineBreaks()
-		fmt.Println()
-
-	},
+func init() {
+	identityCmd := MakeCmd("identity", runIdentity)
+	rootCmd.AddCommand(identityCmd)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// execute prior main
-func init() {
-	rootCmd.AddCommand(identityCmd)
+func runIdentity(cmd *cobra.Command, args []string) {
+	fmt.Println()
+	fmt.Println(IDENT)
+	fmt.Println()
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
