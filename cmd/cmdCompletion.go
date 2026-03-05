9ea47ad39e02e3e3f0c2046d3ed0051bc4d5b19e
Daniel Rivas <danielrivasmd@gmail.com>
2026-03-05 14:17:38 +0100

    refactor completion cmd

--- cmd/cmdCompletion.go
+++ cmd/cmdCompletion.go
@@ -16,92 +16,39 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 package cmd
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 import (
-	"fmt"
 	"os"
 
+	"github.com/DanielRivasMD/horus"
 	"github.com/spf13/cobra"
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// declarations
-var ()
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// completionCmd
-var completionCmd = &cobra.Command{
-	Use:    "completion [bash|zsh|fish|powershell]",
-	Hidden: true,
-	Short:  "Generate completion script.",
-	Long: fmt.Sprintf(`To load completions:
-
-Bash:
-
-  $ source <(%[1]s completion bash)
-
-  # To load completions for each session, execute once:
-  # Linux:
-  $ %[1]s completion bash > /etc/bash_completion.d/%[1]s
-  # macOS:
-  $ %[1]s completion bash > $(brew --prefix)/etc/bash_completion.d/%[1]s
-
-Zsh:
-
-  # If shell completion is not already enabled in your environment,
-  # you will need to enable it.  You can execute the following once:
-
-  $ echo "autoload -U compinit; compinit" >> ~/.zshrc
-
-  # To load completions for each session, execute once:
-  $ %[1]s completion zsh > "${fpath[1]}/_%[1]s"
-
-  # You will need to start a new shell for this setup to take effect.
-
-fish:
-
-  $ %[1]s completion fish | source
-
-  # To load completions for each session, execute once:
-  $ %[1]s completion fish > ~/.config/fish/completions/%[1]s.fish
-
-PowerShell:
-
-  PS> %[1]s completion powershell | Out-String | Invoke-Expression
-
-  # To load completions for every new session, run:
-  PS> %[1]s completion powershell > %[1]s.ps1
-  # and source this file from your PowerShell profile.
-`, rootCmd.Name()),
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	DisableFlagsInUseLine: true,
-	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
-	Args:                  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	Run: func(κ *cobra.Command, args []string) {
-		switch args[0] {
-		case "bash":
-			κ.Root().GenBashCompletion(os.Stdout)
-		case "zsh":
-			κ.Root().GenZshCompletion(os.Stdout)
-		case "fish":
-			κ.Root().GenFishCompletion(os.Stdout, true)
-		case "powershell":
-			κ.Root().GenPowerShellCompletionWithDesc(os.Stdout)
-		}
-	},
+func init() {
+	completionCmd := MakeCmd("completion", runCompletion,
+		WithArgs(cobra.ExactArgs(1)),
+		WithValidArgs([]string{"bash", "zsh", "fish", "powershell"}),
+	)
+	completionCmd.DisableFlagsInUseLine = true // Can't set via options yet
+	rootCmd.AddCommand(completionCmd)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// execute prior main
-func init() {
-	rootCmd.AddCommand(completionCmd)
+func runCompletion(cmd *cobra.Command, args []string) {
+	switch args[0] {
+	case "bash":
+		horus.CheckErr(cmd.Root().GenBashCompletion(os.Stdout))
+	case "zsh":
+		horus.CheckErr(cmd.Root().GenZshCompletion(os.Stdout))
+	case "fish":
+		horus.CheckErr(cmd.Root().GenFishCompletion(os.Stdout, true))
+	case "powershell":
+		horus.CheckErr(cmd.Root().GenPowerShellCompletionWithDesc(os.Stdout))
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
