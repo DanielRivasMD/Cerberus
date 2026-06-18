8225f8b44f7a4d88925c83597f7ae404350b8743
Daniel Rivas <danielrivasmd@gmail.com>
2026-06-18 09:56:37 +0300

    update cmd

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,119 +1,71 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-use crate::cli::{CloneArgs, RememberArgs, StatsArgs, StatusArgs, SyncArgs};
-use crate::util;
-use anyhow::{Result, bail};
-use std::io::Write;
-use std::process::Command;
+pub mod explore;
+pub mod manage;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-pub fn run_clone(args: &CloneArgs) -> Result<()> {
-    let target_dir = args.directory.as_deref().unwrap_or(".");
-    util::clone_repositories_from_csv(&args.csv, target_dir)
-}
+pub mod completion {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
+    use anyhow::Result as anyResult;
+    use clap::{Command, CommandFactory};
+    use clap_complete::{generate, shells::*};
+    use std::io;
 
-pub fn run_describe(verbose: bool) -> Result<()> {
-    let repos = util::collect_repos(None, verbose)?;
-    util::print_describe_table(&repos, None)?;
-    Ok(())
-}
+    use crate::cli;
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
+    pub fn run(shell: cli::Shell) -> anyResult<()> {
+        let visible: Vec<_> = cli::Cli::command()
+            .get_subcommands()
+            .filter(|s| !s.is_hide_set())
+            .cloned()
+            .collect();
 
-pub fn run_readme() -> Result<()> {
-    let cmd_str = r#"
-zellij run --name readme \
-    --close-on-exit --floating \
-    --height 100 --width 130 --x 15 --y 0 \
-    -- zsh -c '
-        file=$(
-            fd --type f --glob "README.md" . \
-            | fzf \
-                --preview="bat --style=plain --color=always {}" \
-                --preview-window="right:70%" \
-                --height=100% \
-                --reverse
-        )
-    [[ -n $file ]] && mdcat --paginate --columns=100 "$file"
-    '"#;
-    let status = Command::new("bash").arg("-c").arg(cmd_str).status()?;
-    if !status.success() {
-        bail!("external command failed");
+        let mut cmd = Command::new(env!("CARGO_BIN_NAME")).subcommands(visible);
+
+        let name = cmd.get_name().to_string();
+
+        match shell {
+            cli::Shell::Bash => generate(Bash, &mut cmd, name, &mut io::stdout()),
+            cli::Shell::Zsh => generate(Zsh, &mut cmd, name, &mut io::stdout()),
+            cli::Shell::Fish => generate(Fish, &mut cmd, name, &mut io::stdout()),
+            cli::Shell::Powershell => generate(PowerShell, &mut cmd, name, &mut io::stdout()),
+        }
+        Ok(())
     }
-    Ok(())
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-pub fn run_roadmap() -> Result<()> {
-    let cmd_str = r#"
-zellij run --name roadmap \
-	--close-on-exit --floating \
-	--height 100 --width 130 --x 15 --y 0 \
-	-- zsh -c '
-		file=$(
-			fd --type f --glob 'ROADMAP.txt' . \
-			| fzf \
-				--preview="bat --style=plain --color=always {}" \
-				--preview-window="right:70%" \
-				--height=100% \
-				--reverse
-		)
-	[[ -n $file ]] && hx "$file"
-    '"#;
-    let status = Command::new("bash").arg("-c").arg(cmd_str).status()?;
-    if !status.success() {
-        bail!("external command failed");
+pub mod identity {
+    use anyhow::Result as anyResult;
+
+    const IDENTITY: &str = r#"In Greek mythology, Cerberus, Κέρβερος, often referred to as the hound of Hades, is a multi-headed dog
+that guards the gates of the underworld to prevent the dead from leaving.
+
+He was the offspring of the monsters Echidna and Typhon, and was usually described as having three heads,
+a serpent for a tail, and snakes protruding from his body.
+
+Cerberus is primarily known for his capture by Heracles, the last of Heracles' twelve labours"#;
+
+    pub fn run() -> anyResult<()> {
+        println!("{}", IDENTITY);
+        Ok(())
     }
-    Ok(())
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-pub fn run_remember(args: RememberArgs, verbose: bool) -> Result<()> {
-    let repos = util::collect_repos(None, verbose)?;
-    let mut writer: Box<dyn Write> = if let Some(path) = &args.output {
-        Box::new(std::fs::File::create(path)?)
-    } else {
-        Box::new(std::io::stdout())
-    };
-    util::write_remember_csv(&repos, &mut writer)?;
-    Ok(())
-}
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-pub fn run_stats(args: StatsArgs, verbose: bool) -> Result<()> {
-    let specific = if args.repo == "." {
-        None
-    } else {
-        Some(args.repo.clone())
-    };
-    let repos = util::collect_repos(specific, verbose)?;
-    util::print_stats_table(&repos, args.year, &args.output)?;
-    Ok(())
-}
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-pub fn run_status(args: StatusArgs, verbose: bool) -> Result<()> {
-    let repos = util::collect_repos(args.repo.clone(), verbose)?;
-    let statuses = util::get_statuses(&repos, args.fetch)?;
-    util::print_status_table(&statuses);
-    Ok(())
-}
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-pub fn run_sync(args: SyncArgs, verbose: bool) -> Result<()> {
-    let repos = util::collect_repos(args.repo.clone(), verbose)?;
-    let action = if args.push { "push" } else { "pull" };
-    let results = util::sync_repos(&repos, args.push, args.pull)?;
-    util::print_sync_table(&results, action);
-    Ok(())
-}
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
